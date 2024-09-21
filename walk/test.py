f1a306366acdf5c31ed8190e589caf1e583eb0a9
Zhiquan Cao <zhiquan.cao@nyu.edu>
2024-09-20 22:48:10 -0400

    cleaned unused variables

--- walk/test.py
+++ walk/test.py
@@ -14,16 +14,12 @@ from lipm import Controller
 m = mujoco.MjModel.from_xml_path('../scene.xml')
 d = mujoco.MjData(m)
 
-
-
 lipm_lqr = Controller()
 
 with mujoco.viewer.launch_passive(m, d) as viewer:
 	# Close the viewer automatically after 30 wall-seconds.
 	start = time.time()
-	# mujoco.set_mjcb_time = True
 	delta_y = 0;
-
 
 	delt = 0
 	step_prev = step_start = 0;
@@ -32,19 +28,10 @@ with mujoco.viewer.launch_passive(m, d) as viewer:
 		viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTFORCE] = 1
 		viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_AUTOCONNECT] = 1
 
-
 	while viewer.is_running() and time.time() - start < 300:
-		elasped = time.time() - start
-
 		# mj_step can be replaced with code that also evaluates
 		# a policy and applies a control signal before stepping the physics.
 		mujoco.mj_step(m, d)
 		lipm_lqr.make_update(d)
-		# print(elasped)
 		# Pick up changes to the physics state, apply perturbations, update options from GUI.
 		viewer.sync()
-
-		# Rudimentary time keeping, will drift relative to wall clock.
-		time_until_next_step = m.opt.timestep - (time.time() - step_start)
-		if time_until_next_step > 0:
-			time.sleep(time_until_next_step)
