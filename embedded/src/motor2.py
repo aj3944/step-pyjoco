04a461e5c7cc71842c04ad9689e38240fad4219b
luka <luka.kitamura@gmail.com>
2025-02-14 14:03:19 -0500

    new motor commands

--- embedded/src/motor2.py
+++ embedded/src/motor2.py
@@ -3,7 +3,17 @@ import time
 import random
 import serial
 
-CANUSB_TTY_BAUD_RATE_DEFAULT = 1000000
+import can
+from can import BusState
+ 
+# can.rc['interface'] = 'serial'
+# can.rc['channel'] = "/dev/tty.usbserial-120"
+# can.rc['baudrate'] = 1000000
+# from can.interface import Bus
+
+# bus = Bus()
+
+CANUSB_TTY_BAUD_RATE_DEFAULT = 2000000
 
 class device():
 	uca = None
@@ -25,3 +35,41 @@ class motor():
 
 	def single_turn_position_control(self):
 		self.device.write(motor_single_turn_position_control(self.motor_id, 0x00))
+
+	def torque_control(self, torque): 
+		# self.device.write(motor_torque_control(self.motor_id))
+		# with can.Bus(interface='serial', channel="/dev/tty.usbserial-120", bitrate=1000000) as bus: 
+		# 	msg = can.Message(
+		# 		arbitration_id=0x141, 
+		# 		# data = [0xA1, 0x00, 0x00, 0x00, 0xAA, 0xAA, 0x00, 0x00],
+		# 		data = [161, 0, 0, 0, 170, 170, 0, 0],
+		# 		is_extended_id=False
+		# 	)
+		# 	try: 
+		# 		bus.send(msg, timeout=2)
+		# 		print(f"Message sent on {bus.channel_info}")
+		# 		print(f"{msg.arbitration_id:X}: {msg.data}")
+				
+		# 	except can.CanError:
+				# print("Message NOT sent")
+		self.device.write(motor_torque_control(self.motor_id, torque))
+
+def receive(): 
+	with can.Bus(interface='serial', channel="/dev/tty.usbserial-120", bitrate=2000000) as bus: 
+		try: 
+			  bus.state = BusState.PASSIVE
+		except NotImplementedError:
+			pass
+
+		try:
+			while True:
+				msg = bus.recv(1)
+				if msg is not None:
+					print(msg)
+
+		except KeyboardInterrupt:
+			pass  # exit normally
+
+if __name__ == '__main__':
+	torque_control()
+	
--- embedded/src/test_2.py
+++ embedded/src/test_2.py
@@ -1,14 +1,16 @@
 from motor2 import device, motor
 import serial
 import time
- 
+
+
 # Initialize the device
 uca = device("/dev/tty.usbserial-130")
 
 test_motor = motor(1, uca.port()) 
 
-test_motor.single_turn_position_control()
+test_motor.torque_control(100)
 
-time.wait(5) 
+time.sleep(5) 
 
 test_motor.stop_motor() 
+time.sleep(1)
