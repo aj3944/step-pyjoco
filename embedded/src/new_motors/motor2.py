642242b12b7ce0594291a1a842c685a23f21744f
Leon <lg3919@nyu.edu>
2025-02-21 14:31:33 -0500

    Update motor2.py

--- embedded/src/new_motors/motor2.py
+++ embedded/src/new_motors/motor2.py
@@ -29,8 +29,41 @@ class motor():
 	def read_mode(self):
 		self.device.write(sys_op_mode(self.motor_id))
 
+	#2.2
+	def write_pid_to_RAM(self, currentPidKp, currentPidKi, speedPidKp, speedPidKi, positionPidKp, positionPidKi):
+		self.device.write(write_pid_to_RAM(self.motor_id, currentPidKp, currentPidKi, speedPidKp, speedPidKi, positionPidKp, positionPidKi))
+
+	#2.4
+	def read_acceleration(self):
+		self.device.write(read_encoder(self.motor_id))
+	
+	#2.6
 	def read_encoder(self):
 		self.device.write(read_encoder(self.motor_id))
+
+	#2.8
+	def read_encoderZero_offset_data(self)
+		self.device.write(read_encoderZero_offset_data(self.motor.id))
+
+	#2.10 
+	def write_encoder_value_to_rom_as_zero(self):
+		self.device.write(write_encoder_value_to_rom_as_zero(self.motor.id))
+
+	#2.12
+	def read_multi_turn_angle(self):
+		self.device.write(read_multi_turn_angle(self.motor.id)
+
+	#2.14
+	def read_motor_stats_one_and_error_flag(self):
+		self.device.write(read_motor_stats_one_and_error_flag(self.motor.id))
+
+	#2.16
+	def read_motor_stats_three(self):
+		self.device.write(read_motor_stats_three(self.motor.id))
+		
+	#2.18
+	def motor_stop(self):
+		self.device.write(motor_stop(self.motor_id))
 
 	# 2.20
     	def speed_control_move(self, speed):
