b8a63dc8398814fdf8ebddcdc92f8f7c449dbedd
lakshyajain06 <jlakshya06@gmail.com>
2025-02-21 14:25:11 -0500

    created wrappers fro 2.1 - 2.11 (odd)

--- embedded/src/new_motors/motor2.py
+++ embedded/src/new_motors/motor2.py
@@ -19,6 +19,26 @@ class motor():
 	def __init__ (self, id, uca): 
 		self.motor_id = id   
 		self.device = uca
+
+	def write_pid_to_rom(self, CurrKP = 0x00, CurrKI = 0x00, SpdKP = 0x00, 
+						SpdKI = 0x00, PosKP = 0x00, PosKI = 0x00):
+		self.device.write(write_pid_rom(self.motor_id, CurrKP, CurrKI, SpdKP,
+								  		 SpdKI, PosKP, PosKI))
+		
+	def write_acceleration(self, function_index, acceleration):
+		self.device.write(write_accel(self.motor_id, function_index, acceleration))
+
+	def read_multi_encoder_orig_pos(self):
+		self.device.write(read_multi_encoder_original_position(self.motor_id))
+
+	def write_multiturn_encoder_zero_rom(self, new_zero):
+		self.device.write(write_encoder_multi_value_rom_as_zero(self.motor_id, new_zero))
+
+	def read_single_turn_encoder(self):
+		self.device.write(read_single_turn_encoder(self.motor_id))
+	
+	def read_pid_param(self):
+		self.device.write(read_pid(self.motor_id))
 		
 	def torque_control_move(self,torque):
 		self.device.write(motor_torque_control(self.motor_id,torque))
